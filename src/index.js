61453b27b47dddf669d8c74e4d223b9cc24d10ee
Marcelo Cardenas <mcl302103@gmail.com>
2024-09-10 16:25:54 -0600

    temas

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from './login';
 import reportWebVitals from './reportWebVitals';
+import Team from './team.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Team />
   </React.StrictMode>
 );
 
