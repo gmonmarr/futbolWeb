c5e7af08a3bd281753304d12de90f25764dfb145
Guillermo Montemayor <A01722402@tec.mx>
2024-09-13 12:31:32 -0600

    cambios al index

--- src/index.js
+++ src/index.js
@@ -5,18 +5,19 @@ import './index.css';
 import Calendar from './pages/Calendar.tsx';
 import Login from './pages/login.js';
 import Team from './pages/team.tsx';
-import CreateTeam from './pages/CreateTeam.tsx'; // Import the CreateTeam page
-import FindTeam from './pages/FindTeam.tsx'; // Import the FindTeam page
-import Admin from './pages/Admin.tsx'; // Import the Admin page
+import CreateTeam from './pages/CreateTeam.tsx';
+import FindTeam from './pages/FindTeam.tsx';
+import Admin from './pages/Admin.tsx';
 import Capitanes from './pages/Capitanes.tsx';
-import ProtectedRoute from './ProtectedRoute'; // Import the ProtectedRoute component
+import ProtectedRoute from './ProtectedRoute';
 import reportWebVitals from './reportWebVitals';
 import LigaDivision from './pages/LigaDivision.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/futbolWeb">
       <Routes>
         <Route path="/" element={<Navigate to="/calendar" />} />
         <Route path="/calendar" element={<Calendar />} />
@@ -25,17 +26,17 @@ root.render(
         <Route path="/create-team" element={<CreateTeam />} />
         <Route path="/find-team" element={<FindTeam />} />
         <Route path="/capitanes" element={<Capitanes />} />
-        <Route path="/capitanes" element={<Capitanes />} />
-        <Route path="/liga-division" element={<LigaDivision />} />        
-        
+        <Route path="/liga-division" element={<LigaDivision />} />
+
         {/* Protected route for Admin */}
         <Route path="/admin" element={
           <ProtectedRoute>
             <Admin />
           </ProtectedRoute>
         } />
+        
+        {/* Fallback route */}
         <Route path="*" element={<Navigate to="/login" />} />
-
       </Routes>
     </Router>
   </React.StrictMode>
